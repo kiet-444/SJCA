02745b3dbfebe7b0a8465a6b5c1b64da2270e722
kiet-444 <kietcao4444@gmail.com>
2025-01-22 10:46:07 +0700

    db

---
+++ migrations/ConnectDB.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: String(process.env.DB_PASSWORD),
+    database: process.env.DB_NAME,
+    dialect: 'postgres' 
+});
+
+sequelize
+    .authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch(err => console.error('Unable to connect to the database:', err));
