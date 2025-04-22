e59b9778779be66a88893f8b254b5ffc2ed51e57
kiet-444 <kietcao4444@gmail.com>
2025-04-22 22:14:25 +0700

    payment

--- models/Payment.js
+++ models/Payment.js
@@ -5,12 +5,16 @@ const Payment = db.define("Payment", {
     id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
     employerId: { type: DataTypes.UUID, allowNull: false },
     workerId: { type: DataTypes.UUID, allowNull: false },
+    jobGroupId: { type: DataTypes.UUID, allowNull: false },
     jobId: { type: DataTypes.UUID, allowNull: false }, // Liên kết công việc
     amount: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
     status: { 
         type: DataTypes.ENUM("PENDING", "HELD", "RELEASED", "CANCELLED"), 
         defaultValue: "PENDING"
     }
-});
+},{
+    timestamps: true
+}
+);
 
 module.exports = Payment;
